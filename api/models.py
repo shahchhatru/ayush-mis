434c4249da2491e0292806eb0e4c2b6f12ffa238
shahchhatru <shahkhesavchhatra@gmail.com>
2024-01-27 18:13:15 +0545

    hello world

--- api/models.py
+++ api/models.py
@@ -135,10 +135,10 @@ class Attendance(models.Model):
 class Teacher(models.Model):
     user = models.OneToOneField(User,null=True,blank=True, on_delete=models.CASCADE)
     name=models.CharField(max_length=100,)
-    email=models.EmailField(max_length=254,unique=True)
+    email=models.EmailField(max_length=254,unique=True,null=True,blank=True)
     address=models.CharField(max_length=100,null=True,blank=True)
     phone=models.CharField(max_length=15,unique=True)
-    post=models.CharField(max_length=15,blank=True)
+    post=models.CharField(max_length=40,null=True,blank=True)
     out_of_department = models.BooleanField(default=False)
     # subject=models.ManyToManyField(Subject)
     def __str__(self):
